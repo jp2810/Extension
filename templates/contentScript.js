bbe8924986c5d5b14344ede0941b73116f40aca5
Jayesh <jayeshrpawar@gmail.com>
2012-01-28 13:08:29 +0530

    extension showing at bottom using div and problem with displaying complaete div on some pages

---
+++ templates/contentScript.js
@@ -0,0 +1,12 @@
+alert("I'm your content script on every page.");
+$(document).ready(function() {
+      alert("document ready occurred!");
+      $('<div class="layout"><div class="mextension"><div class="mextension_images">My extension</div><div class="mextension_video" onclick="show();">video</div><div class="mextension_position">position</div><div class="mextension_person">person</div></div>').appendTo('body');
+});
+
+$(window).load(function() {
+      alert("window load occurred!");
+});
+
+//$('<iframe class="notranslate" src="http://127.0.0.1:5000/iframe_div.html" frameborder="0"  scrolling="no"></iframe>').appendTo('body');
+
