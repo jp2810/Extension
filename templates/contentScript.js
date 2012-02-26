2802c0f8327fc6a79f15c10a308a1379a3d4901f
Jayesh <jayeshrpawar@gmail.com>
2012-02-26 18:59:03 +0530

    added message passing on click of button

--- templates/contentScript.js
+++ templates/contentScript.js
@@ -5,7 +5,7 @@
 $('<div class="popup_keyword1">popup of keyword1</div><div class="popup_keyword2">popup of keyword2</div><div class="popup_keyword3">popup of keyword3</div><div class="popup_keyword4">popup of keyword4</div>').appendTo('body');
 
 //layout of bottom extension
-$('<div class="layout"><div class="mextension"><div class="keyword1"><p id="keyword1">My extension</p></div><div class="keyword2"><p id="keyword2">video</p></div><div class="keyword3"><p id="keyword3">position</p></div><div class="keyword4"><p id="keyword4">person</p></div></div></div>').appendTo('body');
+$('<div class="layout"><div class="mextension"><button type="button" id="prevkeyword">Prev</button><div class="keyword1"><p id="keyword1">My extension</p></div><div class="keyword2"><p id="keyword2">video</p></div><div class="keyword3"><p id="keyword3">position</p></div><div class="keyword4"><p id="keyword4">person</p></div><div class="keyword5"><p  id="keyword5">keyword5</p></div><button type="button" id="nextkeyword">Next</button></div></div>').appendTo('body');
 
 $('.layout').hide();
 $('.popup_keyword1').hide();
@@ -29,3 +29,19 @@ $('.keyword4').click(function(){
 	//alert("in onclick function of keyword4");
 	$('.popup_keyword4').toggle();
 });
+$('#prevkeyword').click(function(){
+	alert("button click");
+	chrome.extension.sendRequest({},function(response){});
+});
+$('#nextkeyword').click(function(){
+	alert("button click");
+	chrome.extension.sendRequest({},function(response){});
+});
+
+
+//1)button click 2)call fuction from background.html  sendrequest/onrequest
+
+/*                   //W
+$(':button').click(function(){
+	alert("button click");
+});*/
